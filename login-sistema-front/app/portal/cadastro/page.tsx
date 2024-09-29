1598c7758f1c21e2c90cdac9b0dbf95172155fc9
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-09-29 14:30:04 -0300

    .

---
+++ login-sistema-front/app/portal/cadastro/page.tsx
@@ -0,0 +1,9 @@
+import SingUp from "@/components/sing_up_form";
+
+export default function SignUpPage() {
+  return (
+    <main className=" flex min-h-screen flex-col items-center p-24">
+      <SingUp/>
+    </main>
+  );
+}
