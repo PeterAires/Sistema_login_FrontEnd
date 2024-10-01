66590fbcd8f18b44a87566a74ca668ba0b1d5e41
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-09-30 22:19:23 -0300

    Feat: add funcionally sing-up form
    Primeira requisição funcional de front-api-back feita 100% por mim

--- login-sistema-front/components/sing_up_form.tsx
+++ login-sistema-front/components/sing_up_form.tsx
@@ -1,12 +1,18 @@
-'use client'
+"use client";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import api from "@/lib/api";
+import { useRouter } from "next/navigation";
+import axios from 'axios'
 
 const SingUp = () => {
+
+  const router = useRouter()
+
   const createAccountFormSchema = z
     .object({
       name: z
@@ -38,36 +44,68 @@ const SingUp = () => {
     resolver: zodResolver(createAccountFormSchema),
   });
 
-  const criarConta = (data: createAccountFormData) => {
-    //requisição http
-    console.log(data)
-  }
+  const criarConta = async (data: createAccountFormData) => {
+    const { name, email, password } = data;
+    try {
+      await  axios.post("http://localhost:3333/portal/cadastro", {
+        name,
+        email,
+        password,
+      });
+      console.log('deu certo')
+      router.push('http://localhost3000/portal/login')
+    } catch {
+      throw new Error("falhou");
+    }
+    
+  };
 
   return (
     <main className=" bg-zinc-900 p-8 rounded-xl flex-col gap-10 text-zinc-300 flex items-center justify-center">
       <h1>Criar Conta</h1>
-      <form onSubmit={handleSubmit(criarConta)} className=" flex-col flex gap-4 w-full max-w-xs">
+      <form
+        onSubmit={handleSubmit(criarConta)}
+        className=" flex-col flex gap-4 w-full max-w-xs"
+      >
         <div className=" flex flex-col gap-1">
           <label>Nome</label>
-          <Input type="" {...register("name")} className=" border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900"/>
+          <Input
+            type=""
+            {...register("name")}
+            className=" border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900"
+          />
           {errors.name && <span>{errors.name.message}</span>}
         </div>
         <div className=" flex flex-col gap-1">
           <label>E-mail</label>
-          <Input type="" {...register("email")} className=" border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900"/>
+          <Input
+            type=""
+            {...register("email")}
+            className=" border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900"
+          />
           {errors.email && <span>{errors.email.message}</span>}
         </div>
         <div className=" flex flex-col gap-1">
           <label>Senha</label>
-          <Input type="" {...register("password")} className=" border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900"/>
+          <Input
+            type=""
+            {...register("password")}
+            className=" border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900"
+          />
           {errors.password && <span>{errors.password.message}</span>}
         </div>
         <div className=" flex flex-col gap-1">
           <label>Conferir senha</label>
-          <Input type="" {...register("checkPassword")} className=" border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900"/>
+          <Input
+            type=""
+            {...register("checkPassword")}
+            className=" border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900"
+          />
           {errors.checkPassword && <span>{errors.checkPassword.message}</span>}
         </div>
-        <Button className="bg-purple-950 rounded font-semibold text-white h-10">Criar conta</Button>
+        <Button className="bg-purple-950 rounded font-semibold text-white h-10">
+          Criar conta
+        </Button>
       </form>
     </main>
   );
