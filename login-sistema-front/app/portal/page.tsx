a59105e9796a431f564f9103ad10a10709d38bf1
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-10-01 18:31:01 -0300

    feat: add veryfication page with token

--- login-sistema-front/app/portal/page.tsx
+++ login-sistema-front/app/portal/page.tsx
@@ -1,11 +1,37 @@
+"use client";
+
+import api from "@/lib/api";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function PortalPage() {
+  const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await api.get("/protected", { withCredentials: true });
+        setIsAuthenticated(true);
+      } catch {
+        router.push("http://localhost:3000/portal/login");
+      }
+    };
+    checkAuth();
+  }, [router]);
+
+  if (!isAuthenticated) {
+    return <p>Loading...</p>
+  }
+
   return (
     <main className=" flex min-h-screen flex-col items-center p-24">
-      <h1 className=" text-xl font-bold text-yellow-300">Pagina do Portal</h1>
+      <h1 className=" text-xl font-bold text-white-300">Pagina do Portal</h1>
 
-      <Link href='api/logout'>logout </Link>
+      <Link href="api/logout" className="text-white">
+        logout{" "}
+      </Link>
     </main>
   );
 }
