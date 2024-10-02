852ad4652752289a19a8da6649215c30b288ca5d
PeterAires <168139612+PeterAires@users.noreply.github.com>
2024-10-02 17:33:41 -0300

    .

--- login-sistema-front/components/ui/input.tsx
+++ login-sistema-front/components/ui/input.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
